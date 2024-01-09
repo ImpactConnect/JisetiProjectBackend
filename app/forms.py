5100af53fc8578932a6060a5db993a8197f99df9
Omotayo <kehindeomotayo11@gmail.com>
2024-01-09 12:09:55 +0100

    Routes Updated

--- app/forms.py
+++ app/forms.py
@@ -11,7 +11,7 @@ class UserRegistration(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()] )
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('ConfirmPassword', validators=[DataRequired(), EqualTo('Password')])
-    phone = IntegerField('Phone', validators=[DataRequired(), Length(min=11, max=11)])
+    phone = IntegerField('Phone', validators=[DataRequired()])
     submit = SubmitField('Sign Up')
     
     def validate_email(self, email):
